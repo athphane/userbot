4e51fb1b948d43bbf085c93c0cfbdf03d49d7d3c
Athfan Khaleel <athphane@gmail.com>
2020-03-06 18:51:24 +0500

    - skyrim image generator

---
+++ userbot/plugins/memes/skyrim.py
@@ -0,0 +1,37 @@
+from userbot import UserBot
+from pyrogram import Filters, Message
+from userbot.helpers.PyroHelpers import ReplyCheck
+import os
+import time
+
+
+@UserBot.on_message(Filters.command(["skyrim"], ".") & Filters.me)
+async def skyrim(bot: UserBot, message: Message):
+    if len(message.command) >= 2:
+        text = message.command[1]
+    else:
+        await message.edit("```Not enough params```")
+        return
+
+    level = message.command[2] if len(message.command) >= 3 else 100
+
+    try:
+        try:
+            if os.name == 'nt':
+                os.system(f"venv\\Scripts\\activate && python userbot\\helpers\\skyrim.py {text} {level}")
+            else:
+                os.system(f"source venv/bin/activate && python userbot\\helpers\\skyrim.py {text} {level}")
+        except:
+            await message.edit("```Failed to generate skill```")
+            time.sleep(2)
+
+        try:
+            await message.reply_photo('userbot/downloads/skyrim.png', reply_to_message_id=ReplyCheck(message))
+            await message.delete()
+        except:
+            await message.edit("```Failed to send skill```")
+            time.sleep(2)
+        finally:
+            os.remove('userbot/downloads/skyrim.png')
+    except Exception as e:
+        print(e)
---
+++ userbot/plugins/scrapers/scrapers.py
@@ -0,0 +1,11 @@
+# import time
+# from userbot import UserBot
+# from userbot.scheduler_system.create_jobs import add_job
+#
+#
+# async def scrape_facts(client: UserBot):
+#     print("Something")
+#
+# add_job(
+#     notify_torrent_complete,
+# )
