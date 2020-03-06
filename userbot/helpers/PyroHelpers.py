39743882204e8f4768cd934dac549dc3ca3edef4
Athfan Khaleel <athphane@gmail.com>
2020-03-06 19:47:46 +0500

    - something

--- userbot/helpers/PyroHelpers.py
+++ userbot/helpers/PyroHelpers.py
@@ -2,15 +2,11 @@ from pyrogram import Message, User
 
 
 def ReplyCheck(message: Message):
-    reply_id = None
-
     if message.reply_to_message:
-        reply_id = message.reply_to_message.message_id
+        return message.reply_to_message.message_id
 
     elif not message.from_user.is_self:
-        reply_id = message.message_id
-
-    return reply_id
+        return message.message_id
 
 
 def SpeedConvert(size):
