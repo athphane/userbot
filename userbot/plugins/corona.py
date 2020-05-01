4db74e2d037b580f622fc2c862b7809390c26837
Athfan Khaleel <athphane@gmail.com>
2020-05-02 03:47:27 +0500

    - style and sleep change

--- userbot/plugins/corona.py
+++ userbot/plugins/corona.py
@@ -1,22 +1,21 @@
+import asyncio
 import requests
 from prettytable import PrettyTable
-from time import sleep
 from userbot import UserBot
 from pyrogram import Filters, Message
 from userbot.plugins.help import add_command_help
 import datetime
 
-@UserBot.on_message(Filters.command(["c"], ".") & Filters.me)
+
+@UserBot.on_message(Filters.command("c", ".") & Filters.me)
 async def corona_all(bot: UserBot, message: Message):
     try:
         r = requests.get("https://corona.lmao.ninja/v2/all?yesterday=true").json()
-        #all_cases = f"<b>Global Totals</b>\nCases: {r['cases']:,}\nDeaths: {r['deaths']:,}\nRecovered: {r['recovered']:,}\nActive: {r['active']:,}"
-        last_updated = datetime.datetime.fromtimestamp(r['updated']/1000).strftime("%Y-%m-%d %I:%M:%S")
-        
+        last_updated = datetime.datetime.fromtimestamp(r['updated'] / 1000).strftime("%Y-%m-%d %I:%M:%S")
+
         ac = PrettyTable()
         ac.header = False
         ac.title = "Global Statistics"
-        
         ac.add_row(["Cases", f"{r['cases']:,}"])
         ac.add_row(["Cases Today", f"{r['todayCases']:,}"])
         ac.add_row(["Deaths", f"{r['deaths']:,}"])
@@ -28,39 +27,37 @@ async def corona_all(bot: UserBot, message: Message):
         ac.add_row(["Deaths/Million", f"{r['deathsPerOneMillion']:,}"])
         ac.add_row(["Tests", f"{r['tests']:,}"])
         ac.add_row(["Tests/Million", f"{r['testsPerOneMillion']:,}"])
-        #ac.add_row(["Affected Countries", f"{r['affectedCountries']:,}"])
         ac.align = "l"
+
         await message.edit(f"```{str(ac)}```\nLast updated on: {last_updated}")
     except Exception as e:
         await message.edit("```The corona API could not be reached```")
         print(e)
-        sleep(3)
+        await asyncio.sleep(3)
         await message.delete()
 
 
-@UserBot.on_message(Filters.command(["cs"], ".") & Filters.me)
+@UserBot.on_message(Filters.command("cs", ".") & Filters.me)
 async def corona_search(bot: UserBot, message: Message):
     cmd = message.command
 
     if not (len(cmd) >= 2):
         await message.edit('```Not enough params provided```')
-        sleep(3)
+        await asyncio.sleep(3)
         await message.delete()
         return
 
     country = cmd[1]
     await message.edit(f"```Getting Corona statistics for {country}```")
 
-    
     r = requests.get(f"https://corona.lmao.ninja/v2/countries/{country}").json()
     if "cases" not in r:
         await message.edit("```The country could not be found!```")
-        sleep(3)
+        await asyncio.sleep(3)
         await message.delete()
     else:
-        #country_cases = f"<b>Cases for {r['country']}</b>\nCases: {r['cases']:,}\nCases Today: {r['todayCases']:,}\nDeaths: {r['deaths']:,}\nDeaths Today: {r['todayDeaths']:,}\nRecovered: {r['recovered']:,}\nActive: {r['active']:,}\nCritical: {r['critical']:,}\nCases/Mil: {r['casesPerOneMillion']}\nDeaths/Mil: {r['deathsPerOneMillion']}"
-        last_updated = datetime.datetime.fromtimestamp(r['updated']/1000).strftime("%Y-%m-%d %I:%M:%S")
-        
+        last_updated = datetime.datetime.fromtimestamp(r['updated'] / 1000).strftime("%Y-%m-%d %I:%M:%S")
+
         cc = PrettyTable()
         cc.header = False
         country = r['countryInfo']['iso3'] if len(r['country']) > 12 else r['country']
@@ -83,7 +80,7 @@ async def corona_search(bot: UserBot, message: Message):
 add_command_help(
     'corona', [
         ['.c', 'Sends global corona stats: cases, deaths, recovered, and active cases'],
-        ['.cs Country',
-         'Sends cases, new cases, deaths, new deaths, recovered, active cases, critical cases, and cases/deaths per one million people for a specific country']
+        ['.cs Country', 'Sends cases, new cases, deaths, new deaths, recovered, active cases, critical cases, '
+                        'and cases/deaths per one million people for a specific country']
     ]
 )
