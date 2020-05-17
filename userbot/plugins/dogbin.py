f236c64158bbd9f8e5ce8a3e8c00fc5e077e0e57
Athfan Khaleel <athphane@gmail.com>
2020-05-17 20:40:55 +0500

    - pastes to nekobin, taken from LyndaRobot

--- userbot/plugins/dogbin.py
+++ userbot/plugins/dogbin.py
@@ -7,17 +7,18 @@ from userbot.plugins.help import add_command_help
 DOGBIN = "https://del.dog/"
 
 
-@UserBot.on_message(Filters.command(['dogbin', 'bin', 'paste'], ".") & Filters.me)
-async def dogbin(bot: UserBot, message: Message):
-    await message.edit_text("`pasting...`")
+@UserBot.on_message(Filters.command(['bin', 'paste'], ".") & Filters.me)
+async def paste(bot: UserBot, message: Message):
+    await message.edit_text("`Pasting...`")
     text = message.reply_to_message.text
     try:
-        paste = requests.post(f"{DOGBIN}/documents", data=text).json()["key"]
+        key = requests.post('https://nekobin.com/api/documents', json={"content": text}).json().get('result').get('key')
     except requests.exceptions.RequestException as e:
         await message.edit_text("`Pasting failed`")
-        print(e)
     else:
-        await message.edit_text(f"{DOGBIN}/{paste}", disable_web_page_preview=True)
+        url = f'https://nekobin.com/{key}'
+        reply_text = f'Nekofied to *Nekobin* : {url}'
+        await message.edit_text(reply_text, parse_mode='markdown', disable_web_page_preview=True)
 
 
 @UserBot.on_message(Filters.command(['getpaste'], ".") & Filters.me)
