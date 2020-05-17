dc034dba08baad1b2b2cd3a9537cb3bec5be0e9c
Athfan Khaleel <athphane@gmail.com>
2020-05-17 20:47:08 +0500

    - remove get paste for now

--- userbot/plugins/dogbin.py
+++ userbot/plugins/dogbin.py
@@ -1,10 +1,7 @@
-import re
 from userbot import UserBot
 from pyrogram import Filters, Message
 import requests
 from userbot.plugins.help import add_command_help
-
-DOGBIN = "https://del.dog/"
 
 
 @UserBot.on_message(Filters.command(['bin', 'paste'], ".") & Filters.me)
@@ -26,28 +23,9 @@ async def paste(bot: UserBot, message: Message):
         )
 
 
-@UserBot.on_message(Filters.command(['getpaste'], ".") & Filters.me)
-async def dogbin(bot: UserBot, message: Message):
-    link = message.command[1]
-    regex = "g\/(\w*)"
-    matches = re.search(re.compile(regex), link)
-    data = requests.get(f"https://del.dog/raw/{matches.group(1)}").text
-
-    await message.edit("Here are the contents from the paste.")
-
-    if len(data) >= 1024:
-        f = open('dogbin.txt', 'w')
-        f.write(data)
-        f.close()
-
-        await message.reply_document('dogbin.txt', caption=f"```Paste from {link}```")
-    else:
-        await message.reply(data)
-
 add_command_help(
-    'dogbin', [
-        ['.dogbin', 'Create a dogbin paste using replied to message.'],
+    'paste', [
+        ['.paste', 'Create a dogbin paste using replied to message.'],
         ['.bin', 'Alternate command #1'],
-        ['.paste', 'Alternate command #2'],
     ]
 )
