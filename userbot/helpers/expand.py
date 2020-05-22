44f51636ca8e1eb6afc6d3c38d2322339477a354
Mohamed Arham <c4.arham@gmail.com>
2020-05-23 01:38:53 +0500

    - We hate async

--- userbot/helpers/expand.py
+++ userbot/helpers/expand.py
@@ -1,9 +1,5 @@
-import requests_async as requests
+import requests
 
-async def expand_url(url):
-    request = await requests.get(f"http://expandurl.com/api/v1/?url={url}")
-    expanded = request.text
-    if expanded != "false":
-        return expanded
-    else:
-        return None
+def expand_url(url):
+    expanded = requests.get(f"http://expandurl.com/api/v1/?url={url}").text
+    return expanded if expanded != "false" else None
