aeeae72e698b39e40c34ecc07ae06e3ae2149064
Mohamed Arham <c4.arham@gmail.com>
2020-05-24 18:36:12 +0500

    - Add music module to search and forward music

---
+++ userbot/plugins/music.py
@@ -0,0 +1,60 @@
+import asyncio
+from userbot import UserBot
+from pyrogram import Filters, Message
+from userbot.helpers.PyroHelpers import ReplyCheck
+from userbot.plugins.help import add_command_help
+
+
+@UserBot.on_message(Filters.command("m", ".") & Filters.me)
+async def send_music(bot: UserBot, message: Message):
+    try:
+        cmd = message.command
+
+        song_name = ""
+        if len(cmd) > 1:
+            song_name = " ".join(cmd[1:])
+        elif message.reply_to_message and len(cmd) is 1:
+            song_name = message.reply_to_message.text
+        elif not message.reply_to_message and len(cmd) is 1:
+            await message.edit("Give a song name")
+            await asyncio.sleep(2)
+            await message.delete()
+            return
+
+        song_results = await bot.get_inline_bot_results("deezermusicbot", song_name)
+        
+        try:
+            #send to Saved Messages because hide_via doesn't work sometimes
+            saved = await bot.send_inline_bot_result(
+                chat_id="me",
+                query_id=song_results.query_id,
+                result_id=song_results.results[0].id,
+                hide_via=True)
+
+            #forward from Saved Messages
+            await bot.forward_messages(
+                chat_id=message.chat.id,
+                from_chat_id="me",
+                message_ids=saved.updates[1].message.id,
+                as_copy=True
+            )
+            #delete the message from Saved Messages
+            await bot.delete_messages("me", saved.updates[1].message.id)
+        except TimeoutError:
+            await message.edit("That didn't work out")
+            await asyncio.sleep(2)
+        await message.delete()
+    except Exception as e:
+        print(e)
+        await message.edit("`Failed to find song`")
+        await asyncio.sleep(2)
+        await message.delete()
+
+
+
+# Command help section
+add_command_help(
+    'music', [
+        ['.m', 'Search songs and send']
+    ]
+)
