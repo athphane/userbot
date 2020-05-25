179148fd0c5b5788a6771f81871a2563ff079709
Athfan Khaleel <athphane@gmail.com>
2020-05-26 00:08:39 +0500

    - animals images consolidated into same handler and dynamic loading

--- userbot/plugins/memes/animals.py
+++ userbot/plugins/memes/animals.py
@@ -2,6 +2,7 @@ import asyncio
 import re
 from time import sleep
 
+import aiohttp
 import requests
 from pyrogram import Filters, Message
 
@@ -11,152 +12,40 @@ from userbot.plugins.help import add_command_help
 
 animal = r"([^.]*)$"
 ok_exts = ["jpg", "jpeg", "png"]
-animals = ['dog', 'cat', 'panda', 'fox', 'bird', 'koala']
+
+animals_data = {
+    'dog': {'url': 'https://random.dog/woof.json', 'key': 'url'},
+    'cat': {'url': 'http://aws.random.cat/meow', 'key': 'file'},
+    'panda': {'url': 'https://some-random-api.ml/img/panda', 'key': 'link'},
+    'redpanda': {'url': 'https://some-random-api.ml/img/red_panda', 'key': 'link'},
+    'bird': {'url': 'https://some-random-api.ml/img/birb', 'key': 'link'},
+    'fox': {'url': 'https://some-random-api.ml/img/fox', 'key': 'link'},
+    'koala': {'url': 'https://some-random-api.ml/img/koala', 'key': 'link'},
+}
+
+animals = [x for x in animals_data]
 
 
-def _prep_dog():
+async def prep_animal_image(animal_data):
     ext = ''
-    dog_pic = None
+    image = None
     while ext not in ok_exts:
-        dog_pic = requests.get('https://random.dog/woof.json').json()['url']
-        ext = re.search(animal, dog_pic).group(1).lower()
-    return dog_pic
+        async with aiohttp.ClientSession() as session:
+            async with session.get(animal_data['url']) as resp:
+                data = await resp.json()
+
+        image = data[animal_data['key']]
+        ext = re.search(animal, image).group(1).lower()
+    return image
 
 
-def _prep_cat():
-    ext = ''
-    cat_pic = None
-    while ext not in ok_exts:
-        cat_pic = requests.get('http://aws.random.cat/meow').json()['file']
-        ext = re.search(animal, cat_pic).group(1).lower()
-    return cat_pic
-
-
-def _prep_panda():
-    ext = ''
-    panda_pic = None
-    while ext not in ok_exts:
-        panda_pic = requests.get('https://some-random-api.ml/img/panda').json()['link']
-        ext = re.search(animal, panda_pic).group(1).lower()
-    return panda_pic
-
-
-def _prep_redpanda():
-    ext = ''
-    redpanda_pic = None
-    while ext not in ok_exts:
-        redpanda_pic = requests.get('https://some-random-api.ml/img/red_panda').json()['link']
-        ext = re.search(animal, redpanda_pic).group(1).lower()
-    return redpanda_pic
-
-
-def _prep_bird():
-    ext = ''
-    bird_pic = None
-    while ext not in ok_exts:
-        bird_pic = requests.get('https://some-random-api.ml/img/birb').json()['link']
-        ext = re.search(animal, bird_pic).group(1).lower()
-    return bird_pic
-
-
-def _prep_fox():
-    ext = ''
-    fox_pic = None
-    while ext not in ok_exts:
-        fox_pic = requests.get('https://some-random-api.ml/img/fox').json()['link']
-        ext = re.search(animal, fox_pic).group(1).lower()
-    return fox_pic
-
-
-def _prep_koala():
-    ext = ''
-    koala_pic = None
-    while ext not in ok_exts:
-        koala_pic = requests.get('https://some-random-api.ml/img/koala').json()['link']
-        ext = re.search(animal, koala_pic).group(1).lower()
-    return koala_pic
-
-
-@UserBot.on_message(Filters.regex("^\.?dog$") & Filters.me)
-async def dog(bot: UserBot, message: Message):
-    if message.from_user.is_self:
-        await message.delete()
-
+@UserBot.on_message((Filters.command(animals, '.') | Filters.command(animals, '')) & Filters.me)
+async def animal_image(bot: UserBot, message: Message):
+    animal_data = animals_data[message.command[0]]
+    await message.delete()
     await bot.send_photo(
         chat_id=message.chat.id,
-        photo=_prep_dog(),
-        reply_to_message_id=ReplyCheck(message)
-    )
-
-
-@UserBot.on_message(Filters.regex("^\.?cat$") & Filters.me)
-async def cat(bot: UserBot, message: Message):
-    if message.from_user.is_self:
-        await message.delete()
-
-    await bot.send_photo(
-        chat_id=message.chat.id,
-        photo=_prep_cat(),
-        reply_to_message_id=ReplyCheck(message)
-    )
-
-
-@UserBot.on_message(Filters.regex("^\.?panda$") & Filters.me)
-async def panda(bot: UserBot, message: Message):
-    if message.from_user.is_self:
-        await message.delete()
-
-    await bot.send_photo(
-        chat_id=message.chat.id,
-        photo=_prep_panda(),
-        reply_to_message_id=ReplyCheck(message)
-    )
-
-
-@UserBot.on_message(Filters.regex("^\.?redpanda$") & Filters.me)
-async def redpanda(bot: UserBot, message: Message):
-    if message.from_user.is_self:
-        await message.delete()
-
-    await bot.send_photo(
-        chat_id=message.chat.id,
-        photo=_prep_redpanda(),
-        reply_to_message_id=ReplyCheck(message)
-    )
-
-
-@UserBot.on_message(Filters.regex("^\.?bird$") & Filters.me)
-async def bird(bot: UserBot, message: Message):
-    if message.from_user.is_self:
-        await message.delete()
-
-    await bot.send_photo(
-        chat_id=message.chat.id,
-        photo=_prep_bird(),
-        reply_to_message_id=ReplyCheck(message)
-    )
-
-
-@UserBot.on_message(Filters.regex("^\.?fox$") & Filters.me)
-async def fox(bot: UserBot, message: Message):
-    if message.from_user.is_self:
-        await message.delete()
-
-    await bot.send_photo(
-        chat_id=message.chat.id,
-        photo=_prep_fox(),
-        reply_to_message_id=ReplyCheck(message)
-    )
-
-
-@UserBot.on_message(Filters.regex("^\.?koala$") & Filters.me)
-async def koala(bot: UserBot, message: Message):
-    if message.from_user.is_self:
-        await message.delete()
-
-    await bot.send_photo(
-        chat_id=message.chat.id,
-        photo=_prep_koala(),
+        photo=await prep_animal_image(animal_data),
         reply_to_message_id=ReplyCheck(message)
     )
 
@@ -189,6 +78,7 @@ async def fact(bot: UserBot, message: Message):
         await asyncio.sleep(2)
         await message.delete()
 
+
 # Command help section
 add_command_help(
     'animals', [
@@ -203,10 +93,10 @@ add_command_help(
     ]
 )
 
-
 fact_help = []
 for x in animals:
-    fact_help.append([f".fact {x}", f"Send a random fact about {x}"])
+    if x is not 'redpanda':
+        fact_help.append([f".fact {x}", f"Send a random fact about {x}"])
 
 add_command_help(
     'facts', fact_help
