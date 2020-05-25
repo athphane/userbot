93cb2bf8db1fdea1ecb70b58c8c8eedfb571ba16
Mohamed Arham <c4.arham@gmail.com>
2020-05-25 05:15:00 +0500

    - Yoda yeeda yada

--- userbot/plugins/translate.py
+++ userbot/plugins/translate.py
@@ -36,9 +36,34 @@ async def translate(bot: UserBot, message: Message):
                              "<b>Translated Text:</b>\n" \
                              f"{result['text'].pop()}"
                 await message.edit(translated)
-        
+
+@UserBot.on_message(Filters.command("yoda", ".") & Filters.me)
+async def yoda(bot: UserBot, message: Message):
+    if message.reply_to_message:
+        txt = message.reply_to_message.text or message.reply_to_message.caption
+    elif len(message.command) > 1:
+        txt = " ".join(message.command[1:])
+    else:
+        await message.edit("Nothing to translate")
+        await asyncio.sleep(3)
+        await message.delete()
+        return
+
+    async with aiohttp.ClientSession() as session:   
+        async with session.get(f"https://api.funtranslations.com/translate/yoda.json?text={txt}") as resp:
+            result = await resp.json()
+
+            if "error" in result:
+                await message.edit(result['error']['message'])
+                await asyncio.sleep(5)
+                await message.delete()
+            else:
+                translated = result['contents']['translated']
+                await message.edit(translated)
+            
 add_command_help(
     'translate', [
-        ['.tr', 'Translate text to English']
+        ['.tr', 'Translate text to English'],
+        ['.yoda', 'Translate text to Yoda']
     ]
 )
