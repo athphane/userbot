6fad87cdb7ab754837d39db197c3fa207cbbd9a1
Athfan Khaleel <athphane@gmail.com>
2020-05-26 16:30:13 +0500

    - omg gif was broken

--- userbot/helpers/file_sending_helpers.py
+++ userbot/helpers/file_sending_helpers.py
@@ -6,7 +6,7 @@ import os.path
 
 
 def reset_file_ids():
-    f = open("file_ids.txt", "w+")
+    f = open("file_ids.txt", "w")
     f.write(json.dumps({}))
     f.close()
 
@@ -27,23 +27,23 @@ async def get_old_message(bot: UserBot, message_id, media_type):
 
 # Save the sent media's ID to file to send faster next time
 def save_media_id(name, media: Message):
-    file_json = json.load(open("file_ids.txt", "r+"))
+    file_json = json.load(open("file_ids.txt", "r"))
     message_id = media.message_id
     file_json[name] = message_id
-    f = open("file_ids.txt", "w+")
+    f = open("file_ids.txt", "w")
     f.write(json.dumps(file_json))
     f.close()
 
 
 # Function to reuse to send animation and remember the file_id
 async def send_saved_animation(bot: UserBot, message: Message, name: str, image: str, caption=None):
-    files = json.load(open("file_ids.txt", "r+"))
+    files = json.load(open("file_ids.txt", "r"))
 
     if name in files:
         old_message = await get_old_message(bot, int(files[name]), "animation")
         if old_message is not None:
             await bot.send_animation(
-                GetChatID(message),
+                message.chat.id,
                 old_message.file_id,
                 file_ref=old_message.file_ref,
                 reply_to_message_id=ReplyCheck(message),
@@ -65,7 +65,7 @@ async def send_saved_animation(bot: UserBot, message: Message, name: str, image:
 
 # Function to reuse to send image and save file_id
 async def send_saved_image(bot: UserBot, message: Message, name: str, image: str, caption=None):
-    files = json.load(open("file_ids.txt", "r+"))
+    files = json.load(open("file_ids.txt", "r"))
 
     if name in files:
         old_message = await get_old_message(bot, int(files[name]), "photo")
@@ -79,7 +79,7 @@ async def send_saved_image(bot: UserBot, message: Message, name: str, image: str
             )
         else:
             # Reset file id list because of the one error
-            # reset_file_ids()
+            reset_file_ids()
             await send_saved_image(bot, message, name, image, caption)
     else:
         sent_photo = await bot.send_photo(
