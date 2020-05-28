ae14bffaa7e88dda47d6304e943ae68d63f35277
Mohamed Arham <c4.arham@gmail.com>
2020-05-28 20:08:10 +0500

    Update music.py

--- userbot/plugins/music.py
+++ userbot/plugins/music.py
@@ -34,7 +34,7 @@ async def send_music(bot: UserBot, message: Message):
                 hide_via=True)
 
             # forward from Saved Messages
-            reply_to = message.reply_to_message.message_id
+            reply_to = message.reply_to_message.message_id if message.reply_to_message else None
             await bot.send_audio (
                 chat_id=message.chat.id,
                 audio=saved.updates[1].media.document.id,
