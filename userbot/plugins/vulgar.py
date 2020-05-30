3fa58db34e72b83189e4992e2a926b689b2879f3
Athfan Khaleel <athphane@gmail.com>
2020-05-31 02:26:52 +0500

    - haha you cant say the n-word anymore

---
+++ userbot/plugins/vulgar.py
@@ -0,0 +1,21 @@
+import re
+
+from pyrogram import Filters, Message
+
+from userbot import UserBot
+
+bad_words = ['nigga', 'nigger', 'coon']
+
+
+@UserBot.on_message(Filters.me, group=10)
+async def i_am_not_allowed_to_say_this(bot: UserBot, message: Message):
+    txt = message.caption if message.caption else message.text
+
+    for word in bad_words:
+        txt = re.sub(word, 'bruh', txt, flags=re.IGNORECASE)
+
+    if txt != message.text:
+        try:
+            await message.edit(txt)
+        except:
+            await message.edit_caption(txt)
