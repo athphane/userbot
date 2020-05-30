4c913cead12208e5c0cd590bd2d847ba32551ea2
Athfan Khaleel <athphane@gmail.com>
2020-05-31 02:36:12 +0500

    - fucks sake

--- userbot/plugins/vulgar.py
+++ userbot/plugins/vulgar.py
@@ -9,13 +9,17 @@ bad_words = ['nigga', 'nigger', 'coon']
 
 @UserBot.on_message(Filters.me, group=10)
 async def i_am_not_allowed_to_say_this(bot: UserBot, message: Message):
-    txt = message.caption if message.caption else message.text
+    if message.caption:
+        txt = message.caption
+    elif message.text:
+        txt = message.text
 
     for word in bad_words:
         txt = re.sub(word, 'bruh', txt, flags=re.IGNORECASE)
 
-    if txt != message.caption if message.caption else message.text:
-        try:
-            await message.edit(txt)
-        except:
+    if message.caption:
+        if txt != message.caption:
             await message.edit_caption(txt)
+    elif message.text:
+        if txt != message.text:
+            await message.edit(txt)
