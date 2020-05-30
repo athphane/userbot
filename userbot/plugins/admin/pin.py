791c1ac1b3448f044f36708122cbbb27aedd93bd
Athfan Khaleel <athphane@gmail.com>
2020-05-31 02:08:12 +0500

    - lets pin some shit

---
+++ userbot/plugins/admin/pin.py
@@ -0,0 +1,62 @@
+import asyncio
+
+from pyrogram import Filters, Message
+from pyrogram.client.methods.chats.get_chat_members import Filters as ChatMemberFilters
+
+from userbot import UserBot
+from userbot.plugins.help import add_command_help
+
+
+@UserBot.on_message(Filters.command('pin', '.') & Filters.me)
+async def pin_message(bot: UserBot, message: Message):
+    # First of all check if its a group or not
+    if message.chat.type in ['group', 'supergroup']:
+        # Here lies the sanity checks
+        admins = await bot.get_chat_members(message.chat.id, filter=ChatMemberFilters.ADMINISTRATORS)
+        admin_ids = [user.user.id for user in admins]
+        me = await bot.get_me()
+
+        # If you are an admin
+        if me.id in admin_ids:
+            # If you replied to a message so that we can pin it.
+            if message.reply_to_message:
+                disable_notification = True
+
+                # Let me see if you want to notify everyone. People are gonna hate you for this...
+                if len(message.command) >= 2 and message.command[1] in ['alert', 'notify', 'loud']:
+                    disable_notification = False
+
+                # Pin the fucking message.
+                pinned_event = await bot.pin_chat_message(
+                    message.chat.id,
+                    message.reply_to_message.message_id,
+                    disable_notification=disable_notification
+                )
+                await message.edit("`Pinned message!`")
+
+                return
+            else:
+                # You didn't reply to a message and we can't pin anything. ffs
+                await message.edit(
+                    f"`Reply to a message so that I can pin the god damned thing...`")
+        else:
+            # You have no business running this command.
+            await message.edit("`I am not an admin here lmao. What am I doing?`")
+    else:
+        # Are you fucking dumb this is not a group ffs.
+        await message.edit("`This is not a place where I can pin shit.`")
+
+    # And of course delete your lame attempt at changing the group picture.
+    # RIP you.
+    # You're probably gonna get ridiculed by everyone in the group for your failed attempt.
+    # RIP.
+    await asyncio.sleep(3)
+    await message.delete()
+
+
+# Command help section
+add_command_help(
+    'admin', [
+        ['.pin', 'Pin the replied to message.']
+    ]
+)
