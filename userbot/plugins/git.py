d6c037b02d72f8066f0aadec28c1f91d9470589b
Mohamed Arham <c4.arham@gmail.com>
2020-05-30 21:16:59 +0500

    - Maaro Mujhe Maaro

--- userbot/plugins/git.py
+++ userbot/plugins/git.py
@@ -19,8 +19,8 @@ async def last_commit(bot: UserBot, message: Message):
     commit_link = f"<a href='https://github.com/athphane/userbot/commit/{id}'>{id[:7]}</a>"
     author = master.commit.author.name
     date_time = datetime.datetime.fromtimestamp(master.commit.committed_date)
-    commit_msg = f"**Latest commit**:\n{commit_link}\n\n**Commit Message**:\n```{commit.strip()}```\n\n**By**: ```{author}```\n\n**On**: ```{date_time}```"
-    await message.edit(commit_msg)
+    commit_msg = f"**Latest commit**: {commit_link}\n\n**Commit Message**:\n```{commit.strip()}```\n\n**By**: ```{author}```\n\n**On**: ```{date_time}```"
+    await message.edit(commit_msg, disable_web_page_preview=True)
 
 # Command help section
 add_command_help(
