279706c4afb6b9f137d947a4518f6720db8de8df
Mohamed Arham <c4.arham@gmail.com>
2020-05-30 21:21:07 +0500

    - Hmm maybe this will fix it

--- userbot/plugins/git.py
+++ userbot/plugins/git.py
@@ -19,7 +19,7 @@ async def last_commit(bot: UserBot, message: Message):
     commit_link = f"<a href='https://github.com/athphane/userbot/commit/{id}'>{id[:7]}</a>"
     author = master.commit.author.name
     date_time = datetime.datetime.fromtimestamp(master.commit.committed_date)
-    commit_msg = f"**Latest commit**: {commit_link}\n\n**Commit Message**:\n```{commit.strip()}```\n\n**By**: ```{author}```\n\n**On**: ```{date_time}```"
+    commit_msg = f"**Latest commit**: {commit_link}\n\n**Commit Message**:\n```{commit.strip()}```\n\n**By**: `{author}`\n\n**On**: `{date_time}`"
     await message.edit(commit_msg, disable_web_page_preview=True)
 
 # Command help section
