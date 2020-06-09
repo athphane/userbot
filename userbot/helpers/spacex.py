0a2aef63e95126ada2cf6aade8678630fb409968
Mohamed <c4.arham@gmail.com>
2020-06-09 16:52:03 +0500

    - Added .nspacex to get next spaceX launch details
    Signed-off-by: Mohamed <c4.arham@gmail.com>

--- userbot/helpers/spacex.py
+++ userbot/helpers/spacex.py
@@ -5,3 +5,9 @@ async def get_latest():
         async with session.get("https://api.spacexdata.com/v3/launches/latest") as resp:
             data = await resp.json()
     return data
+
+async def get_next():
+    async with aiohttp.ClientSession() as session:
+        async with session.get("https://api.spacexdata.com/v3/launches/next") as resp:
+            data = await resp.json()
+    return data
--- userbot/plugins/spacex.py
+++ userbot/plugins/spacex.py
@@ -4,12 +4,12 @@ from pyrogram import Filters, Message
 
 from userbot import UserBot
 from userbot.helpers.PyroHelpers import ReplyCheck
-from userbot.helpers.spacex import get_latest
+from userbot.helpers.spacex import get_latest, get_next
 from userbot.plugins.help import add_command_help
 
 
 @UserBot.on_message(Filters.command(['spacex', 'elon', 'ae12'], '.') & Filters.me)
-async def nice(bot: UserBot, message: Message):
+async def spacex(bot: UserBot, message: Message):
     await message.delete()
 
     data = await get_latest()
@@ -46,9 +46,32 @@ async def nice(bot: UserBot, message: Message):
             disable_web_page_preview=True
         )
 
+
+@UserBot.on_message(Filters.command(['.nspacex', 'nextlaunch'   ], '.') & Filters.me)
+async def nextlaunch(bot: UserBot, message: Message):
+    await message.delete()
+
+    data = await get_next()
+
+    dt = datetime.utcfromtimestamp(int(data['launch_date_unix'])).strftime('%d-%m-%Y %H:%M:%S')
+
+    txt = f"<b>Mission Name:</b> {data['mission_name']}\n" \
+          f"<b>Flight No:</b> {data['flight_number']}\n" \
+          f"<b>Rocket Name:</b> {data['rocket']['rocket_name']}\n" \
+          f"<b>Launch Site:</b> {data['launch_site']['site_name']}\n" \
+          f"<b>Launch Date:</b> {dt}\n\n" \
+          f"\n\n{data['details']}"
+    
+    await bot.send_message(
+        chat_id=message.chat.id,
+        text=txt,
+        disable_web_page_preview=True
+    )
+
 # Command help section
 add_command_help(
     'spacex', [
-        ['.spacex', 'Get the latest launch details']
+        ['.spacex', 'Get the latest launch details'],
+        ['.nspacex', 'Get the next launch details']
     ]
 )
