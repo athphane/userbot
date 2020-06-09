916590d6dde779a521cdacc15d8f57a8e6627017
Athfan Khaleel <athphane@gmail.com>
2020-06-09 20:36:36 +0500

    - spacex helpers spacing

--- userbot/helpers/spacex.py
+++ userbot/helpers/spacex.py
@@ -1,10 +1,12 @@
 import aiohttp
 
+
 async def get_latest():
     async with aiohttp.ClientSession() as session:
         async with session.get("https://api.spacexdata.com/v3/launches/latest") as resp:
             data = await resp.json()
     return data
+
 
 async def get_next():
     async with aiohttp.ClientSession() as session:
