eaafbd177c0fba501b89e0801934bb3f8644a00a
Mohamed <c4.arham@gmail.com>
2020-06-11 21:53:07 +0500

    - Add .hmention
    Signed-off-by: Mohamed <c4.arham@gmail.com>

--- userbot/plugins/mention.py
+++ userbot/plugins/mention.py
@@ -28,10 +28,28 @@ async def mention_user(bot: UserBot, message: Message):
     _mention = mention(user.id, ' '.join(message.command[2:]))
     await message.edit(_mention)
 
+@UserBot.on_message(Filters.command("hmention", ".") & Filters.me)
+async def hidden_mention(bot: UserBot, message: Message):
+    if len(message.command) < 2:
+        await message.edit("Incorrect format\nExample: .hmention @Athfan")
+        await asyncio.sleep(3)
+        await message.delete()
+        return
+    try:
+        user = await bot.get_users(message.command[1])
+    except Exception:
+        await message.edit("User not found")
+        await asyncio.sleep(3)
+        await message.delete()
+        return
+    
+    _mention = mention(user.id, '\u200B')
+    await message.edit(_mention)
 
 # Command help section
 add_command_help(
     'mention', [
         ['.mention', 'Mention a user with a different name\nExample: `.mention @Athfan CTO`'],
+        ['.hmention', 'Mention a user with a hidden text\nExample: `.hmention @Athfan`']
     ]
 )
