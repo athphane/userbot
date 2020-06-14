78890d14f74bba2a2c60d8f23c479e573c3991be
Poki <aman@virtual.mv>
2020-06-14 23:08:57 +0500

    Revert "Revert since we dont need"
    This reverts commit f18c71deabfdebe32922a0316e678e0cde15c91b.

--- userbot/database/__init__.py
+++ userbot/database/__init__.py
@@ -1,15 +1,20 @@
 import pymongo
 
-from userbot import MONGO_URL, DB_USERNAME, DB_PASSWORD, DB_NAME
+from userbot import MONGO_URL, DB_USERNAME, DB_PASSWORD, DB_NAME, IS_ATLAS
 
 
 def database():
     """Created Database connection"""
-    client = pymongo.MongoClient(
-        MONGO_URL,
-        username=DB_USERNAME,
-        password=DB_PASSWORD
-    )
+    if IS_ATLAS:
+        client = pymongo.MongoClient(
+            MONGO_URL,
+        )
+    else:
+        client = pymongo.MongoClient(
+            MONGO_URL,
+            username=DB_USERNAME,
+            password=DB_PASSWORD
+        )
     db = client[DB_NAME]
     return db
 
