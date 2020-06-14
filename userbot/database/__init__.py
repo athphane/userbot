f18c71deabfdebe32922a0316e678e0cde15c91b
Poki <aman@virtual.mv>
2020-06-14 22:07:05 +0500

    Revert since we dont need

--- userbot/database/__init__.py
+++ userbot/database/__init__.py
@@ -1,20 +1,15 @@
 import pymongo
 
-from userbot import MONGO_URL, DB_USERNAME, DB_PASSWORD, DB_NAME, IS_ATLAS
+from userbot import MONGO_URL, DB_USERNAME, DB_PASSWORD, DB_NAME
 
 
 def database():
     """Created Database connection"""
-    if IS_ATLAS:
-        client = pymongo.MongoClient(
-            MONGO_URL,
-        )
-    else:
-        client = pymongo.MongoClient(
-            MONGO_URL,
-            username=DB_USERNAME,
-            password=DB_PASSWORD
-        )
+    client = pymongo.MongoClient(
+        MONGO_URL,
+        username=DB_USERNAME,
+        password=DB_PASSWORD
+    )
     db = client[DB_NAME]
     return db
 
