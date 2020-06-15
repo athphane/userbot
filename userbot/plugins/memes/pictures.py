1a65d6272da5ee52be1a95b11a4c855178c166e1
Mohamed Arham <c4.arham@gmail.com>
2020-06-15 23:21:14 +0500

    - lmao

--- userbot/plugins/memes/pictures.py
+++ userbot/plugins/memes/pictures.py
@@ -7,7 +7,7 @@ from userbot.helpers.PyroHelpers import ReplyCheck
 from userbot.plugins.help import add_command_help
 
 @UserBot.on_message(Filters.command('person', '.') & Filters.me)
-async def send_person(bot: BOT, message: Message):
+async def send_person(bot: UserBot, message: Message):
     await bot.send_photo(
         chat_id=message.chat.id,
         photo="https://thispersondoesnotexist.com/image?randomtag=" + str(randint(0,9999)),
