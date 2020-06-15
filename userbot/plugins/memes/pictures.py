d64784ac4bd2985692f910d04d3a36ae226ea204
Mohamed Arham <c4.arham@gmail.com>
2020-06-15 23:49:52 +0500

    - I wrote this in github.com don't @ me

--- userbot/plugins/memes/pictures.py
+++ userbot/plugins/memes/pictures.py
@@ -1,4 +1,6 @@
 from random import randint
+import aiohttp
+import shutil
 
 from pyrogram import Filters, Message
 
@@ -6,11 +8,28 @@ from userbot import UserBot
 from userbot.helpers.PyroHelpers import ReplyCheck
 from userbot.plugins.help import add_command_help
 
+async def get_person():
+    rand_int = str(randint(0,9999))
+    url = "https://thispersondoesnotexist.com/image?randomtag=" + rand_int
+    async with aiohttp.ClientSession(auto_decompress=false) as session:
+        async with session.get(url) as resp:
+            content = await resp.content.read()
+            file_name = f"{rand_int}person.jpeg"
+            with open(file_name, 'wb') as file:
+                shutil.copyfileobj(content, file)
+    return file_name
+
 @UserBot.on_message(Filters.command('person', '.') & Filters.me)
 async def send_person(bot: UserBot, message: Message):
+    try:
+        photo = await get_person()
+    except Exception as err:
+        await message.edit("Error occured while fetching random person")
+        return
+    
     await bot.send_photo(
         chat_id=message.chat.id,
-        photo="https://thispersondoesnotexist.com/image?randomtag=" + str(randint(0,9999)),
+        photo=photo,
         caption="persono",
         reply_to_message_id=ReplyCheck(message)
     )
