bd1813a18f2dda9658f601ad50e72ef8d1df7a0a
Athfan Khaleel <athphane@gmail.com>
2020-06-16 14:16:58 +0500

    - random cat lah

--- userbot/plugins/thispersonandcatdoesnotexist.py
+++ userbot/plugins/thispersonandcatdoesnotexist.py
@@ -2,7 +2,7 @@ import os
 from random import randint
 
 from pyrogram import Filters, Message
-from thisapidoesnotexist import get_person
+from thisapidoesnotexist import get_person, get_cat
 
 from userbot import UserBot
 from userbot.helpers.PyroHelpers import ReplyCheck
@@ -13,6 +13,16 @@ async def this_person_does_no_exist(bot: UserBot, message: Message):
     person = get_person()
     file_name = f"{randint(1, 999)}person.jpeg"
     person.save_image(file_name)
+    await bot.send_photo(message.chat.id, file_name, reply_to_message_id=ReplyCheck(message))
+    os.remove(file_name)
+    await message.delete()
+
+
+@UserBot.on_message((Filters.command('rcat', '.') & Filters.me))
+async def this_person_does_no_exist(bot: UserBot, message: Message):
+    cat = get_cat()
+    file_name = f"{randint(1, 999)}cat.jpeg"
+    cat.save_image(file_name)
     await bot.send_photo(message.chat.id, file_name, reply_to_message_id=ReplyCheck(message))
     os.remove(file_name)
     await message.delete()
