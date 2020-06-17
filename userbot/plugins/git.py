36ce904b1ee5ebd59e603da4d7d0b435c9135961
Mohamed Arham <c4.arham@gmail.com>
2020-06-17 16:14:09 +0500

    - Github graph (#34)
    * - Edited get_raw (#33)
    
    * - Adding github commit graph

--- userbot/plugins/git.py
+++ userbot/plugins/git.py
@@ -2,11 +2,18 @@ import datetime
 import os
 
 import git
+import cairosvg
+import aiofiles
+from random import randint
+from glob import iglob
+
 from pyrogram import Filters, Message
+from asyncio import sleep
 
 from userbot import UserBot
+from userbot.helpers.PyroHelpers import ReplyCheck
 from userbot.plugins.help import add_command_help
-
+from userbot.helpers.aiohttp import AioHttp
 
 @UserBot.on_message(Filters.command(['lastcommit', 'lc'], '.') & Filters.me)
 async def last_commit(bot: UserBot, message: Message):
@@ -20,10 +27,45 @@ async def last_commit(bot: UserBot, message: Message):
     commit_msg = f"**Latest commit**: {commit_link}\n\n**Commit Message**:\n```{commit.strip()}```\n\n**By**: `{author}`\n\n**On**: `{date_time}`"
     await message.edit(commit_msg, disable_web_page_preview=True)
 
+@UserBot.on_message(Filters.command(['ggraph', 'commitgraph'], '.') & Filters.me)
+async def commit_graph(bot: UserBot, message: Message):
+    if len(message.command) < 2:
+        message.edit("Please provide a github profile username to generate the graph!")
+        await sleep(2)
+        message.delete()
+        return
+    else:
+        git_user = message.command[1]
+    
+    url = f"https://ghchart.rshah.org/{git_user}"
+    resp = AioHttp.get_raw(url)
+    f = await aiofiles.open('git.svg', mode='wb')
+    await f.write(resp)
+    await f.close()
+    
+    file_name = f"{randint(1,999)}{git_user}"
+    try:
+        cairosvg.svg2png(url=f"{file_name}.svg", write_to=f"{file_name}.png")
+    except UnboundLocalError:
+        message.edit("Username does not exist!")
+        await sleep(2)
+        message.delete()
+        return
+    
+    await bot.send_photo(
+        chat_id=message.chat.id,
+        photo="git.png",
+        caption=git_user,
+        reply_to_message_id=ReplyCheck(message)
+    )
+    
+    for file in iglob(f"{file_name}.*"):
+        os.remove(file)
 
 # Command help section
 add_command_help(
     'git', [
         ['.lastcommit | .lc', 'Gets the last commit message.'],
+        ['.ggraph | .commitgraph', 'Gets the commit graph for a Github user.'],
     ]
 )
