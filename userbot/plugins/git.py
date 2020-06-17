a8bdc2df55eb90608c186add20e127a65e3aaa2e
Athfan Khaleel <athphane@gmail.com>
2020-06-18 02:23:15 +0500

    - PEP8

--- userbot/plugins/git.py
+++ userbot/plugins/git.py
@@ -1,19 +1,19 @@
 import datetime
 import os
-
-import git
-import cairosvg
-import aiofiles
-from random import randint
-from glob import iglob
-
-from pyrogram import Filters, Message
 from asyncio import sleep
+from glob import iglob
+from random import randint
+
+import aiofiles
+import cairosvg
+import git
+from pyrogram import Filters, Message
 
 from userbot import UserBot
 from userbot.helpers.PyroHelpers import ReplyCheck
-from userbot.plugins.help import add_command_help
 from userbot.helpers.aiohttp import AioHttp
+from userbot.plugins.help import add_command_help
+
 
 @UserBot.on_message(Filters.command(['lastcommit', 'lc'], '.') & Filters.me)
 async def last_commit(bot: UserBot, message: Message):
@@ -27,6 +27,7 @@ async def last_commit(bot: UserBot, message: Message):
     commit_msg = f"**Latest commit**: {commit_link}\n\n**Commit Message**:\n```{commit.strip()}```\n\n**By**: `{author}`\n\n**On**: `{date_time}`"
     await message.edit(commit_msg, disable_web_page_preview=True)
 
+
 @UserBot.on_message(Filters.command(['ggraph', 'commitgraph'], '.') & Filters.me)
 async def commit_graph(bot: UserBot, message: Message):
     if len(message.command) < 2:
@@ -36,16 +37,15 @@ async def commit_graph(bot: UserBot, message: Message):
         return
     else:
         git_user = message.command[1]
-    
-    
+
     url = f"https://ghchart.rshah.org/{git_user}"
-    file_name = f"{randint(1,999)}{git_user}"
-    
+    file_name = f"{randint(1, 999)}{git_user}"
+
     resp = await AioHttp.get_raw(url)
     f = await aiofiles.open(f"{file_name}.svg", mode='wb')
     await f.write(resp)
     await f.close()
-    
+
     try:
         cairosvg.svg2png(url=f"{file_name}.svg", write_to=f"{file_name}.png")
     except UnboundLocalError:
@@ -53,7 +53,7 @@ async def commit_graph(bot: UserBot, message: Message):
         await sleep(2)
         await message.delete()
         return
-    
+
     await bot.send_photo(
         chat_id=message.chat.id,
         photo=f"{file_name}.png",
@@ -61,9 +61,10 @@ async def commit_graph(bot: UserBot, message: Message):
         reply_to_message_id=ReplyCheck(message)
     )
     await message.delete()
-    
+
     for file in iglob(f"{file_name}.*"):
         os.remove(file)
+
 
 # Command help section
 add_command_help(
