cbd6b562b42e0c8b957e299e15bf8cbe7f3ddc72
Athfan Khaleel <athphane@gmail.com>
2020-06-21 01:40:32 +0500

    - Re-definition found for builtin function FIX

--- userbot/plugins/git.py
+++ userbot/plugins/git.py
@@ -22,11 +22,12 @@ async def last_commit(_, message: Message):
     repo = git.Repo(os.getcwd())
     master = repo.head.reference
     commit = master.commit.message.strip()
-    id = master.commit.hexsha
-    commit_link = f"<a href='https://github.com/athphane/userbot/commit/{id}'>{id[:7]}</a>"
+    commit_id = master.commit.hexsha
+    commit_link = f"<a href='https://github.com/athphane/userbot/commit/{commit_id}'>{commit_id[:7]}</a>"
     author = master.commit.author.name
     date_time = datetime.datetime.fromtimestamp(master.commit.committed_date)
-    commit_msg = f"**Latest commit**: {commit_link}\n\n**Commit Message**:\n```{commit.strip()}```\n\n**By**: `{author}`\n\n**On**: `{date_time}`"
+    commit_msg = f"**Latest commit**: {commit_link}\n\n**Commit Message**:\n```{commit.strip()}```\n\n" \
+                 f"**By**: `{author}`\n\n**On**: `{date_time}`"
     await message.edit(commit_msg, disable_web_page_preview=True)
 
 
