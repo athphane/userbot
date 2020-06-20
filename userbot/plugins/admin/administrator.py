8526562cc8e9e618047cd4b23e65473800124d66
Tiashe <52987409+Tiashe@users.noreply.github.com>
2020-06-20 12:39:36 +0500

    added user mention

--- userbot/plugins/admin/administrator.py
+++ userbot/plugins/admin/administrator.py
@@ -2,6 +2,7 @@ from pyrogram import Filters, Message
 from pyrogram.errors import UserAdminInvalid
 
 from userbot import UserBot
+from userbot.helpers.PyroHelpers import GetUserMentionable
 from userbot.helpers.adminHelpers import (
     CheckAdmin, CheckReplyAdmin, Timer, TimerString, RestrictFailed
 )
@@ -10,17 +11,18 @@ from userbot.helpers.adminHelpers import (
 @UserBot.on_message(Filters.command("ban", prefixes='.') & Filters.me)
 async def ban_hammer(bot: UserBot, message: Message):
     if await CheckReplyAdmin(message) is True:
-        if await CheckAdmin(bot, message) is True:
+        if await CheckAdmin(message) is True:
             try:
+                mention = await GetUserMentionable(message.from_user)
                 await bot.kick_chat_member(
                     chat_id=message.chat.id,
                     user_id=message.reply_to_message.from_user.id,
                     until_date=Timer(message)
                 )
                 if len(message.command) > 1:
-                    await message.edit(f"{user_id} has been banned for {until_date}.")
+                    await message.edit(f"{mention} has been banned for {until_date}.")
                 else:
-                    await message.edit(f"{user_id} has been banned indefinitely.")
+                    await message.edit(f"{mention} has been banned indefinitely.")
             except UserAdminInvalid:
                 await RestrictFailed(message)
 
@@ -28,13 +30,14 @@ async def ban_hammer(bot: UserBot, message: Message):
 @UserBot.on_message(Filters.command("unban", prefixes='.') & Filters.me)
 async def unban(bot: UserBot, message: Message):
     if await CheckReplyAdmin(message) is True:
-        if await CheckAdmin(bot, message) is True:
+        if await CheckAdmin(message) is True:
             try:
+                mention = await GetUserMentionable(message.from_user)
                 await bot.unban_chat_member(
                     chat_id=message.chat.id,
                     user_id=message.reply_to_message.from_user.id
                 )
-                await message.edit(f"Congratulations {user_id} you have been unbanned." 
+                await message.edit(f"Congratulations {mention} you have been unbanned." 
                                    "Follow the rules and be careful from now on.")
             except UserAdminInvalid:
                 await message.edit("I can't unban this user.")
@@ -43,8 +46,9 @@ async def unban(bot: UserBot, message: Message):
 @UserBot.on_message(Filters.command("mute", prefixes='.') & Filters.me)
 async def mute_hammer(bot: UserBot, message: Message):
     if await CheckReplyAdmin(message) is True:
-        if await CheckAdmin(bot, message) is True:
+        if await CheckAdmin(message) is True:
             try:
+                mention = await GetUserMentionable(message.from_user)
                 await bot.restrict_chat_member(
                     chat_id=message.chat.id,
                     user_id=message.reply_to_message.from_user.id,
@@ -52,9 +56,9 @@ async def mute_hammer(bot: UserBot, message: Message):
                     can_send_messages=False,
                 )
                 if len(message.command) > 1:
-                    await message.edit(f"{user_id} has been banned for {until_date}.")
+                    await message.edit(f"{mention} has been banned for {until_date}.")
                 else:
-                    await message.edit(f"{user_id} has been banned indefinitely.")
+                    await message.edit(f"{mention} has been banned indefinitely.")
             except UserAdminInvalid:
                 await RestrictFailed(message)
 
@@ -62,8 +66,9 @@ async def mute_hammer(bot: UserBot, message: Message):
 @UserBot.on_message(Filters.command("unmute", prefixes='.') & Filters.me)
 async def unmute(bot: UserBot, message: Message):
     if await CheckReplyAdmin(message) is True:
-        if await CheckAdmin(bot, message) is True:
+        if await CheckAdmin(message) is True:
             try:
+                mention = await GetUserMentionable(message.from_user)
                 await bot.restrict_chat_member(
                     chat_id=message.chat.id,
                     user_id=message.reply_to_message.from_user.id,
@@ -77,25 +82,26 @@ async def unmute(bot: UserBot, message: Message):
                     can_invite_users=True,
                     can_pin_messages=True
                 )
-                await message.edit(f"{user_id}, you may send messages here now.")
+                await message.edit(f"{mention}, you may send messages here now.")
             except UserAdminInvalid:
                 await RestrictFailed(message)
 
 
 @UserBot.on_message(Filters.command("kick", prefixes='.') & Filters.me)
 async def kick_user(bot: UserBot, message: Message):
-    if await CheckReplyAdmin(message) is True:
-        if await CheckAdmin(bot, message) is True:
+    if CheckReplyAdmin(message) is True:
+        if CheckAdmin(message) is True:
             try:
-                await bot.kick_chat_member(
+                mention = await GetUserMentionable(message.from_user)
+                bot.kick_chat_member(
                     chat_id=message.chat.id,
                     user_id=message.reply_to_message.from_user.id,
                     until_date=0
                 )
-                await bot.unban_chat_member(
+                bot.unban_chat_member(
                     chat_id=message.chat.id,
                     user_id=message.reply_to_message.from_user.id
                 )
-                await message.edit(f"{user_id}, Sayonara motherfucker.")
+                await message.edit(f"{mention}, Sayonara motherfucker.")
             except UserAdminInvalid:
                 await RestrictFailed(message)
