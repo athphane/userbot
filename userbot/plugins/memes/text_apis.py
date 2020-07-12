af66cee5f905f7fd4dc09f0ff07075104f2f0638
Athfan Khaleel <athphane@gmail.com>
2020-07-12 23:24:58 +0500

    - do a thing

--- userbot/plugins/memes/text_apis.py
+++ userbot/plugins/memes/text_apis.py
@@ -9,7 +9,7 @@ text_apis_data = {
     'compliment': {'url': 'https://complimentr.com/api', 'target_key': 'compliment'},
     'devexcuse': {'url': 'https://dev-excuses-api.herokuapp.com/', 'target_key': 'text'},
     'insult': {'url': 'https://evilinsult.com/generate_insult.php?lang=en', 'target_key': 'insult'},
-    'kanye': {'url': 'https://api.kanye.rest/', 'target_key': 'quote'},
+    'kanye': {'url': 'https://api.kanye.rest/', 'target_key': 'quote', 'format': 'Kanye once said:\n `{}'},
 }
 
 text_apis = [x for x in text_apis_data]
@@ -25,8 +25,12 @@ async def text_api(_, message: Message):
         try:
             data = await AioHttp().get_json(api['url'])
             resp_json = data[api['target_key']]
+            if 'format' in api:
+                txt = api['format'].format(resp_json)
+            else:
+                txt = resp_json.capitalize()
             await message.edit(
-                resp_json.capitalize()
+                txt
             )
         except Exception:
             data = await AioHttp().get_text(api['url'])
