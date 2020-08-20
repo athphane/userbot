955a03cf32fc236fd5053e7279c581f9c8ea2f55
Tiashe <52987409+Tiashe@users.noreply.github.com>
2020-08-21 00:17:07 +0500

    fixed bug in qr generator

--- userbot/plugins/qr generator.py
+++ userbot/plugins/qr generator.py
@@ -14,18 +14,17 @@ async def generate_qr(_, m: Message):
     try:
         if os.path.exists('userbot/downloads/qr.png'):
             os.remove('userbot/downloads/qr.png')
-        else:
-            qr_object = m.text[3:]
-            async with aiohttp.ClientSession() as session:
-                url = f"https://api.qrserver.com/v1/create-qr-code/?size=150x150&data={qr_object}"
-                async with session.get(url) as resp:
-                    if resp.status == 200:
-                        f = await aiofiles.open('userbot/downloads/qr.png', mode='wb')
-                        await f.write(await resp.read())
-                        await f.close()
-            await m.edit_text("Generating QR...")
-            await UserBot.send_photo(m.chat.id, 'userbot/downloads/qr.png')
-            await m.delete()
+        qr_object = m.text[3:]
+        async with aiohttp.ClientSession() as session:
+            url = f"https://api.qrserver.com/v1/create-qr-code/?size=150x150&data={qr_object}"
+            async with session.get(url) as resp:
+                if resp.status == 200:
+                    f = await aiofiles.open('userbot/downloads/qr.png', mode='wb')
+                    await f.write(await resp.read())
+                    await f.close()
+        await m.edit_text("Generating QR...")
+        await UserBot.send_photo(m.chat.id, 'userbot/downloads/qr.png')
+        await m.delete()
     except exception as e:
         print(e)
 
