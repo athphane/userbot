8c48920a3d6993fc6ce0aca9fb877d8c9495618f
Tiashe <52987409+Tiashe@users.noreply.github.com>
2020-08-22 00:11:33 +0500

    finishing touches

--- userbot/plugins/qr generator.py
+++ userbot/plugins/qr generator.py
@@ -14,20 +14,19 @@ from userbot.plugins.help import add_command_help
 @UserBot.on_message(Filters.command("qr", prefixes=".") & Filters.me)
 async def generate_qr(_, m: Message):
     try:
-        if os.path.exists('userbot/downloads/qr.png'):
-            os.remove('userbot/downloads/qr.png')
-        else:
-            qr_object = m.text[3:]
-            async with aiohttp.ClientSession() as session:
-                url = f"https://api.qrserver.com/v1/create-qr-code/?size=150x150&data={qr_object}"
-                async with session.get(url) as resp:
-                    if resp.status == 200:
-                        f = await aiofiles.open('userbot/downloads/qr.png', mode='wb')
-                        await f.write(await resp.read())
-                        await f.close()
-            await m.edit_text("Generating QR...")
-            await UserBot.send_photo(m.chat.id, 'userbot/downloads/qr.png')
-            await m.delete()
+        if os.path.exists('userbot/downloads/qr.png'):  
+            os.remove('userbot/downloads/qr.png')       
+        qr_object = m.text[3:]               
+        async with aiohttp.ClientSession() as session:
+            url = f"https://api.qrserver.com/v1/create-qr-code/?size=150x150&data={qr_object}"
+            async with session.get(url) as resp:
+                if resp.status == 200:
+                    f = await aiofiles.open('userbot/downloads/qr.png', mode='wb')
+                    await f.write(await resp.read())
+                    await f.close()
+        await m.edit_text("Generating QR...")
+        await Bot.send_photo(m.chat.id, 'userbot/downloads/qr.png')
+        await m.delete()
     except exception as e:
         print(e)
 
@@ -40,7 +39,6 @@ async def qr_decode(_, m:Message):
         await m.edit_text("Decoding QR...")
         output = await read_qr(image)
         await m.edit_text(output)
-
     except exception as e:
         await UserBot.send_message(m.chat.id, f"Oopsie I encountered an error: {e}.")
         print(e)
@@ -57,5 +55,6 @@ async def read_qr(image):
 add_command_help(
     'qr', [
         ['.qr', 'Generates a qr image of anything you want.\nUsage: `.qr example`'],
+        ['.decode', 'Reply to a qr image to decode it.'],
     ]
 )
