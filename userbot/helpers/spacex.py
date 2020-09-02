2edc7cb8d072bdc6636dbd145837ea7ac9fd0049
Mohamed Arham <c4.arham@gmail.com>
2020-09-02 18:36:29 +0500

    - Big pp energy

--- userbot/helpers/spacex.py
+++ userbot/helpers/spacex.py
@@ -3,15 +3,8 @@ import aiohttp
 
 class Spacex:
     @staticmethod
-    async def get_latest():
+    async def get(latest_or_next):
         async with aiohttp.ClientSession() as session:
-            async with session.get("https://api.spacexdata.com/v3/launches/latest") as resp:
-                data = await resp.json()
-        return data
-
-    @staticmethod
-    async def get_next():
-        async with aiohttp.ClientSession() as session:
-            async with session.get("https://api.spacexdata.com/v3/launches/next") as resp:
+            async with session.get(f"https://api.spacexdata.com/v3/launches/{latest_or_next}") as resp:
                 data = await resp.json()
         return data
