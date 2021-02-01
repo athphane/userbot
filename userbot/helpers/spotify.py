e2e3de515a4830c8dd6db296bb2a410965fad34e
Mohamed Arham <c4.arham@gmail.com>
2021-02-01 14:23:20 +0500

    - Add check for api details

--- userbot/helpers/spotify.py
+++ userbot/helpers/spotify.py
@@ -6,6 +6,8 @@ redirect_uri = "http://localhost:8888/callback"
 scope = 'user-read-currently-playing'
 
 async def now_playing():
+    if [x for x in (SPOTIFY_USERNAME, SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET) if x is None]:
+        return "API details not set"
     token = util.prompt_for_user_token(
         SPOTIFY_USERNAME, scope, SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET, redirect_uri)
 
