3adbcccab2da77a56389d35740fbec604ae214da
Mohamed <c4.arham@gmail.com>
2021-03-04 23:57:29 +0500

    - Monthly bullshit commits

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -108,7 +108,7 @@ scheduler = AsyncIOScheduler()
 redirect_uri = "http://localhost:8888/callback"
 scope = 'user-read-currently-playing'
 
-if [x for x in (SPOTIFY_USERNAME, SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET) if x]:
+if [x for x in (SPOTIFY_USERNAME, SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET) if x is not None]:
     token = util.prompt_for_user_token(
         SPOTIFY_USERNAME, scope, SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET, redirect_uri)
 
