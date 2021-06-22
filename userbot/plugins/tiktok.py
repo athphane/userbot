1bcf9222886194e9f5bcafc8a4c7fdd7827374fd
Athfan Khaleel <athphane@gmail.com>
2021-06-22 18:43:26 +0500

    - test

--- userbot/plugins/tiktok.py
+++ userbot/plugins/tiktok.py
@@ -5,6 +5,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from userbot import UserBot
+from userbot.helpers.PyroHelpers import ReplyCheck
 from userbot.helpers.tiktokHelper import TikTok
 from userbot.plugins.help import add_command_help
 
@@ -24,18 +25,15 @@ async def download_tiktok(bot: UserBot, message: Message):
     try:
         await message.edit("Processing link...")
         tiktok_video = await TikTok.download_tiktok(txt)
-        await bot.send_video(message.chat.id, tiktok_video, caption=txt, supports_streaming=True)
-        await message.delete()
-        os.remove(tiktok_video)
-        return
-
+        await bot.send_video(message.chat.id, tiktok_video, caption=txt, reply_to_message_id=ReplyCheck(message),
+                             supports_streaming=True)
     except Exception as e:
         print(e)
+        os.remove(tiktok_video)
         await message.edit("Error while processing tiktok link")
         await asyncio.sleep(3)
-        await message.delete()
-        return
 
+    await message.delete()
 
 add_command_help(
     "tiktok",
