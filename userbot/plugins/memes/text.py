a46de0c12468d589242a78250c0887b96d72b61a
Athfan Khaleel <athphane@gmail.com>
2021-07-24 13:20:08 +0500

    english is not my first language

--- userbot/plugins/memes/text.py
+++ userbot/plugins/memes/text.py
@@ -233,7 +233,7 @@ async def heads_tails(_, message: Message):
 
 @UserBot.on_message(filters.command(["otherwise", 'other'], ".") & filters.me)
 async def youd_think_so_but_this_says_otherwise(_, message: Message):
-    await message.edit(f"You\'d thing so, but this says <a href='https://i.imgur.com/nzncews.jpg'>otherwise</a>.")
+    await message.edit(f"You\'d think so, but this says <a href='https://i.imgur.com/nzncews.jpg'>otherwise</a>.")
 
 
 @UserBot.on_message(filters.command("reverset", ".") & filters.me)
