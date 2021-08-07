548be1238f301cd0b281c533c213694ce61db60e
Athfan Khaleel <athphane@gmail.com>
2021-08-07 22:12:42 +0500

    - increased degeneracy

--- userbot/plugins/sticker_sender.py
+++ userbot/plugins/sticker_sender.py
@@ -8,6 +8,11 @@ SETS = {
         'CAACAgUAAxkDAAI4IGEOvOVBnkzKf6MQCgvsJuGk5PRbAAKYAgACf1BBV4xU-M-oahBJHgQ',
         'CAACAgUAAxkDAAI4IWEOvOZKgAYskiJ3gcu6InBogwnkAAJKAwACmd1BV4c0knnsItszHgQ',
         'CAACAgUAAxkDAAI4ImEOvOmcilCfnLmTxOGZj5Liv0t5AAIVBAACvM9BV9WweboNia0rHgQ'
+    ],
+    2: [
+        'CAACAgUAAxkDAAI4J2EOvtgOW67X-ozbSRS_0ubjEJEMAAIbAwACcYtQV1-l_18Yq80SHgQ',
+        'CAACAgUAAxkDAAI4KGEOvtnG2TMNUKYOLkof4g0Yp5hSAAJKAgACGM5RV20DZ7YyvyeCHgQ',
+        'CAACAgUAAxkDAAI4KWEOvtkP_NTgkg4GobiDa9pNJPfXAAI9AwACrd9RVx3nZiYeQ4PeHgQ'
     ]
 }
 
@@ -26,6 +31,6 @@ async def sticker_sender(bot: UserBot, message: Message):
             await bot.send_sticker(message.chat.id, x)
 
 
-# @UserBot.on_message(filters.sticker & filters.me)
-# async def sticker_sender(_, message: Message):
-#     print(message.sticker.file_id)
+@UserBot.on_message(filters.sticker & filters.me)
+async def sticker_sender(_, message: Message):
+    print(message.sticker.file_id)
