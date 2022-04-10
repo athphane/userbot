df4b304301f1e2ba0e2a7dfdc5d4cbbeaa98c2f2
Mohamed Arham <c4.arham@gmail.com>
2022-04-11 04:10:20 +0800

    - Fix security bug

--- userbot/plugins/admin/administrator.py
+++ userbot/plugins/admin/administrator.py
@@ -98,7 +98,7 @@ unmute_permissions = ChatPermissions(
     can_send_other_messages=True,
     can_add_web_page_previews=True,
     can_send_polls=True,
-    can_change_info=True,
+    can_change_info=False,
     can_invite_users=True,
     can_pin_messages=False,
 )
