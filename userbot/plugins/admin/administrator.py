db2506dbb128a3745e23dc62ce86ccd0f17ff6de
Mohamed Arham <c4.arham@gmail.com>
2022-04-16 21:15:37 +0800

    - fix visual bug

--- userbot/plugins/admin/administrator.py
+++ userbot/plugins/admin/administrator.py
@@ -93,7 +93,7 @@ async def mute_hammer(bot: UserBot, message: Message):
 
 # Unmute permissions
 unmute_permissions = ChatPermissions(
-     can_send_messages=True,
+    can_send_messages=True,
     can_send_media_messages=True,
     can_send_other_messages=True,
     can_add_web_page_previews=True,
