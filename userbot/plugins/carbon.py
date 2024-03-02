67c72bcc911b4b9bd1b346d80a9b32070a98b4ad
i701 <idhaan701@outlook.com>
2024-03-02 12:47:05 +0500

    fix carbon plugin by using carbown now api instead of cli and remove language setter and getter functions

--- userbot/plugins/carbon.py
+++ userbot/plugins/carbon.py
@@ -5,9 +5,11 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from userbot import UserBot
+from userbot.helpers.carbon_client import carbon_client
 from userbot.plugins.help import add_command_help
 
 CARBON_LANG = "py"
+CODE_TO_CARBONIZE = ""
 
 
 @UserBot.on_message(filters.command("carbon", ".") & filters.me)
@@ -17,62 +19,77 @@ async def carbon_test(bot: UserBot, message: Message):
     Eg: .carbon your code here (multi line supported)
     """
     carbon_text = message.text[8:]
-
-    # Write the code to a file cause carbon-now-cli wants a file.
-    file = "userbot/downloads/carbon.{}".format(get_carbon_lang())
-    with open(file, "w+") as f:
-        f.write(carbon_text)
-
-    await message.edit_text("Carbonizing code...")
-    # Do the thing
-    os.system("carbon-now -h -t userbot/downloads/carbon {}".format(file))
-    # await message.edit_text("Carbonizing completed...")
-    # Send the thing
-    await bot.send_photo(message.chat.id, "userbot/downloads/carbon.png")
-    await message.delete()
-
-
-@UserBot.on_message(filters.command("carbonlang", ".") & filters.me)
-async def update_carbon_lang(bot: UserBot, message: Message):
-    """
-    Set language to use Carbon with.
-    Eg: .carbonlang js -> will set the file type to js
-    """
-    global CARBON_LANG
-    cmd = message.command
-
-    type_text = ""
-    if len(cmd) > 1:
-        type_text = " ".join(cmd[1:])
-    elif message.reply_to_message and len(cmd) == 1:
-        type_text = message.reply_to_message.text
-    elif not message.reply_to_message and len(cmd) == 1:
-        await message.edit("Give me something to carbonize")
-        await sleep(2)
-        await message.delete()
+    if message.reply_to_message and message.reply_to_message.text:
+        CODE_TO_CARBONIZE = message.reply_to_message.text
+    elif len(carbon_text) > 0:
+        CODE_TO_CARBONIZE = message.command[1]
+    else:
+        # top secret https://tinyurl.com/Donbestupid
+        await message.edit_text(
+            "Where's the code bruh? Reply to a code or provide it after `.carbon`"
+        )
         return
 
-    CARBON_LANG = type_text
-    await message.edit_text("Carbon type set to {}".format(type_text))
-    await sleep(2)
+    await message.edit_text("Carbonizing code...")
+    """
+    the carbon client could be customized with message.commands
+    like `.carbon yeti` (to set what theme user wants)
+    but because `.carbon <code>` without replying needs to work also it is kinda complicated i think to make it work
+    """
+
+    # Do the thing boom
+    client = carbon_client()
+    img = await client.create(CODE_TO_CARBONIZE)
+    await message.edit_text("Carbonizing completed...")
+    # Send the thing
+    await bot.send_photo(message.chat.id, img)
     await message.delete()
+    # Remove the generated image after sending it
+    os.remove(img)
 
 
-@UserBot.on_message(filters.command("carbonlang", "!") & filters.me)
-async def send_carbon_lang(bot: UserBot, message: Message):
-    """
-    Edits message to show current set carbon language
-    """
-    await message.edit_text(get_carbon_lang())
-    await sleep(5)
-    await message.delete()
+# since the carbon api has auto language detection i thought this is not necessary anymore
+# @UserBot.on_message(filters.command("carbonlang", ".") & filters.me)
+# async def update_carbon_lang(bot: UserBot, message: Message):
+#     """
+#     Set language to use Carbon with.
+#     Eg: .carbonlang js -> will set the file type to js
+#     """
+#     global CARBON_LANG
+#     cmd = message.command
+
+#     type_text = ""
+#     if len(cmd) > 1:
+#         type_text = " ".join(cmd[1:])
+#     elif message.reply_to_message and len(cmd) == 1:
+#         type_text = message.reply_to_message.text
+#     elif not message.reply_to_message and len(cmd) == 1:
+#         await message.edit("Give me something to carbonize")
+#         await sleep(2)
+#         await message.delete()
+#         return
+
+#     CARBON_LANG = type_text
+#     await message.edit_text("Carbon type set to {}".format(type_text))
+#     await sleep(2)
+#     await message.delete()
 
 
-def get_carbon_lang():
-    """
-    Gets carbon language. Default py
-    """
-    return CARBON_LANG
+# @UserBot.on_message(filters.command("carbonlang", "!") & filters.me)
+# async def send_carbon_lang(bot: UserBot, message: Message):
+#     """
+#     Edits message to show current set carbon language
+#     """
+#     await message.edit_text(get_carbon_lang())
+#     await sleep(5)
+#     await message.delete()
+
+
+# def get_carbon_lang():
+#     """
+#     Gets carbon language. Default py
+#     """
+#     return CARBON_LANG
 
 
 add_command_help(
@@ -82,15 +99,15 @@ add_command_help(
             ".carbon",
             "Generates a carbon image of your code.\nUsage: `.carbon` reply to message or command args",
         ],
-        [
-            ".carbonlang",
-            "Change carbon language for syntax highlighting.\nUsage: `.carbonlang` reply to message or "
-            "command args\n"
-            "Please use file extensions for best results.",
-        ],
-        [
-            "!carbonlang",
-            "Show current carbon language. Default is python.\nUsage: `!carbonlang`",
-        ],
+        # [
+        #     ".carbonlang",
+        #     "Change carbon language for syntax highlighting.\nUsage: `.carbonlang` reply to message or "
+        #     "command args\n"
+        #     "Please use file extensions for best results.",
+        # ],
+        # [
+        #     "!carbonlang",
+        #     "Show current carbon language. Default is python.\nUsage: `!carbonlang`",
+        # ],
     ],
 )
